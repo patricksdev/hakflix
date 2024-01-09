91afcbd8451fdee78425546755a0bdf6b7756385
Patrick <patricks.dev@icloud.com>
2024-01-09 13:24:39 +0100

    jojo

--- src/app/featured-film.tsx
+++ src/app/featured-film.tsx
@@ -1,15 +1,23 @@
 import Image from 'next/image';
 import thumb from '../../public/images/thumb.webp';
+import thumb_title from '../../public/images/thumb_title.webp';
 
 export default function FeaturedFilm() {
 	return (
-		<div className='relative w-full h-[100vh] max-sm:h-[50vh] -mt-9'>
+		<div className='relative w-full h-[100vh] max-sm:h-[50vh]'>
+			<div className='dark-overlay h-[100vh] max-sm:h-[50vh]'></div>
 			<Image
 				src={thumb}
 				alt='Featured Film'
-				className='w-full h-[100vh] object-cover max-sm:h-[50vh]'
+				className='w-full h-[100vh] object-cover max-sm:h-[50vh] -mt-14'
 			/>
-			<div className='dark-overlay h-[100vh] max-sm:h-[50vh]'></div>
+			<div className=''>
+				<Image
+					src={thumb_title}
+					alt='Thumb Title'
+					className='absolute top-14 z-10'
+				/>
+			</div>
 		</div>
 	);
 }
--- src/app/navigation.tsx
+++ src/app/navigation.tsx
@@ -3,15 +3,20 @@ import hakflix from '../../public/logos/hakflix.svg';
 
 export default function Navigation() {
 	return (
-		<div className='sticky h-auto left-0 top-0 flex z-10 items-center'>
+		<div className='nav sticky h-14 left-0 top-0 flex z-10 items-center px-14 gap-12 pt-4'>
 			<Image
 				src={hakflix}
 				alt='Hakflix'
-				className='h-9 w-auto'
+				className='h-7 w-auto'
 			/>
-			<div className='flex'>
-				<a>Home</a>
-				<a>Popular</a>
+			<div className='flex text-sm gap-5'>
+				<a
+					href='#'
+					className='font-medium'
+				>
+					Home
+				</a>
+				<a href='#'>Popular</a>
 			</div>
 		</div>
 	);
