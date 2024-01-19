3016f2bb958c3d726a5687582f0593974e1ab64b
Patrick <patrick.live@icloud.com>
2024-01-19 22:26:40 +0100

    added route support without parent

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -42,7 +42,7 @@ export default function Home() {
 				<div className="titles-overlay absolute"></div>
 				<Shows />
 			</div>
-			<Footer />
+			{/* <Footer /> */}
 		</div>
 	);
 }
--- src/app/shows/shows.tsx
+++ src/app/shows/shows.tsx
@@ -24,7 +24,6 @@ export default function Shows() {
 	const [rndShows, setrndShows] = useState<hakflixShow[] | []>();
 
 	useEffect(() => {
-		console.log('SHOWS');
 		if (shows) setrndShows(shuffle(shows));
 	}, []);
 
@@ -89,6 +88,30 @@ export default function Shows() {
 							</SwiperSlide>
 						))}
 				</Swiper>
+			</div>
+			<div className="footer w-full h-fit flex justify-center items-center bottom-0 pt-20 pb-20 px-14">
+				<div className="text-center flex flex-col gap-6 max-sm:gap-6 items-center">
+					<p className="text-sm text-center flex w-2/4 max-sm:w-full leading-normal">
+						Hakflix ist eine rein fiktive und satirische Webseite, welche
+						in keinerlei Verbindung mit dem Streamingdienst Netflix steht.
+						Die hier dargestellten Inhalte stammen ursprünglich aus einer
+						offiziellen Maturazeitung. Hakflix übernimmt keine
+						Verantwortung für missverstandene Inhalte oder etwaige
+						Konsequenzen, die aus der Nutzung dieser Parodie-Plattform
+						entstehen könnten.
+					</p>
+
+					<span className="text-sm">
+						Made with<span className="heart-color"> &#9829; </span>by{' '}
+						<Link
+							href="https://github.com/patricksdev"
+							target="_blank"
+							className="underline"
+						>
+							patricks.dev
+						</Link>
+					</span>
+				</div>
 			</div>
 		</div>
 	);
